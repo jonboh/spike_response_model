22ffde1194d65b9195e00f20c375a3fa018eef23
Jonbo <jon.bosque.hernando@gmail.com>
2019-07-15 17:30:19 +0200

    optim from excitatory to inhibitory neuron

--- optimize.py
+++ optimize.py
@@ -5,53 +5,51 @@ import matplotlib.pyplot as plt
 
 from scipy.optimize import fmin_cg
 
+import simulation
+
 
 def error_fun(u):
     return np.sum((u_data - u)**2)
 
-def error_kappa(kappa):
-    u = np.matmul(I, kappa)
+def error_param(param):
+    kappa = param[0:filter_lenght]
+    eta = param[filter_lenght:]
+    u = np.matmul(I, kappa) + np.matmul(A, eta)
     return error_fun(u)
 
 
-t_sim = 1000
-t = np.arange(0,t_sim)
+steps = 1000
+t = np.arange(0,steps)
+i = np.ones((steps,))
 
-u_data = np.zeros((t_sim,))
-u_data[2] = 1
-u_data[4] = 2
-u_data[7] = 1
+filter_lenght = 100
 
-i = np.ones((t_sim,))/3
-i[0] = 0 
-i[300:] = 0
+neuron_gen = simulation.spr_neuron(filter_lenght, 'e')
 
-a = np.zeros((t_sim,))
-a[75] = 1
-a[250] = 1
+u_data, a, _ = neuron_gen.simulate(i)
 
+neuron_fit = simulation.spr_neuron(filter_lenght, 'i')
 
-kappa = 0.45*np.exp(-t/18)
-# Original: eta = 7*(np.exp(-t/18)-np.exp(-t/45))
-eta = 7*(np.exp(-(t-1)/18)-np.exp(-(t-1)/45))
-eta[0] = 15
-theta1 = 12*np.exp(-t/37)+2*np.exp(-t/500)
 
 # u = I * k
-I = np.nan * np.ones((t_sim, len(kappa)))
+I = np.nan * np.ones((steps, filter_lenght))
+A = np.nan * np.ones((steps, filter_lenght))
 for n in range(I.shape[0]):
-    if n<len(kappa):
-        i_n = np.concatenate((i[n::-1], np.zeros((len(kappa)-n-1))), axis=0)
+    if n<filter_lenght:
+        i_n = np.concatenate((i[n::-1], np.zeros((filter_lenght-n-1))), axis=0)
+        a_n = np.concatenate((a[n::-1], np.zeros((filter_lenght-n-1))), axis=0) 
     else:
-        i_n = i[n::-1][0:k]
+        i_n = i[n::-1][0:filter_lenght]
+        a_n = a[n::-1][0:filter_lenght]
     I[n,:] = i_n
+    A[n,:] = a_n
 
 iterations = 2
 for _ in range(iterations):
 
     
-    u_vec = np.matmul(I, kappa)
-    u = np.convolve(kappa, i, mode='full') + np.convolve(eta, a, mode='full')
+    u_vec = np.matmul(I, neuron_fit.kappa) + np.matmul(A, neuron_fit.eta)
+    u = np.convolve(neuron_fit.kappa, i, mode='full') + np.convolve(neuron_fit.eta, a, mode='full')
     error = error_fun(u_vec)
 
     print('error: {0}'.format(error))
@@ -68,19 +66,19 @@ for _ in range(iterations):
 
     # Filters
     plt.subplot(4,2,3)
-    plt.plot(t, kappa, label = 'kappa(s)')
+    plt.plot(neuron_fit.s, neuron_fit.kappa, label = 'kappa(s)')
     #plt.title('kappa(s)')
     plt.legend()
     plt.subplot(4,2,4)
-    plt.plot(t, eta, label='eta(s)')
+    plt.plot(neuron_fit.s, neuron_fit.eta, label='eta(s)')
     #plt.title('eta(s)')
     plt.legend()
 
     # Membrane Potential
     plt.subplot(4,1,3)
-    #plt.plot(u_data, label='data')
-    plt.plot(u[0:t_sim], label='u(t) = conv(I, k) + conv(eta, a)')
-    plt.plot(u_vec, label='u(t) = I * k')
+    plt.plot(u_data, label='data')
+    plt.plot(u[0:steps], label='u(t) = conv(I, k) + conv(eta, a)')
+    plt.plot(u_vec, label='u(t) = I * kappa + A * eta')
     #plt.title('u(t)')
     plt.legend()
 
@@ -90,4 +88,8 @@ for _ in range(iterations):
     plt.legend()
     plt.show()
 
-    kappa = fmin_cg(error_kappa, kappa)
+    param = np.concatenate((neuron_fit.kappa, neuron_fit.eta), axis=0)
+    param = fmin_cg(error_param, param)
+    neuron_fit.kappa = param[0:filter_lenght]
+    neuron_fit.eta = param[filter_lenght:]
+

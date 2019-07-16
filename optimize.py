1021a48b6f935602c9e96162bc56267f9e38349b
Jonbo <jon.bosque.hernando@gmail.com>
2019-07-16 20:13:39 +0200

    Use Newton-CG optimization with Jacobian and Hessian.

--- optimize.py
+++ optimize.py
@@ -8,6 +8,18 @@ from scipy.optimize import minimize
 
 import simulation
 
+def numerical_gradient(param):
+    e = error_u_param(param)
+    eps = 10**-8
+    J_approx = np.nan * np.ones((2*filter_lenght,))
+    for i in range(len(J_approx)):
+        diff = np.zeros(J_approx.shape)
+        diff[i] += eps
+        J_approx[i] = (error_u_param(param+diff) - e)/eps
+    return J_approx
+          
+def u_compute(kappa, eta):
+    return np.matmul(I, kappa) + np.matmul(A, eta)
 
 def error_u(u):
     return np.sum((u_data - u)**2)
@@ -15,20 +27,20 @@ def error_u(u):
 def error_u_param(param):
     kappa = param[0:filter_lenght]
     eta = param[filter_lenght:]
-    u = np.matmul(I, kappa) + np.matmul(A, eta)
+    u = u_compute(kappa, eta)
     return error_u(u)
 
 
 steps = 300
 t = np.arange(0,steps)
-i = np.ones((steps,))
-i[0:10] = 0
+intensity = np.ones((steps,))
+#intensity[0:1] = 0
 
 filter_lenght = 15
 
 neuron_gen = simulation.spr_neuron(filter_lenght, 'e')
 
-u_data, a_data, _ = neuron_gen.simulate(i)
+u_data, a_data, _ = neuron_gen.simulate(intensity)
 
 neuron_fit = simulation.spr_neuron(filter_lenght, 'i')
 
@@ -38,21 +50,48 @@ I = np.nan * np.ones((steps, filter_lenght))
 A = np.nan * np.ones((steps, filter_lenght))
 for n in range(I.shape[0]):
     if n<filter_lenght:
-        i_n = np.concatenate((i[n::-1], np.zeros((filter_lenght-n-1))), axis=0)
+        i_n = np.concatenate((intensity[n::-1], np.zeros((filter_lenght-n-1))), axis=0)
         a_n = np.concatenate((a_data[n::-1], np.zeros((filter_lenght-n-1))), axis=0) 
     else:
-        i_n = i[n::-1][0:filter_lenght]
+        i_n = intensity[n::-1][0:filter_lenght]
         a_n = a_data[n::-1][0:filter_lenght]
     I[n,:] = i_n
     A[n,:] = a_n
 
-iterations = 100
-for _ in range(iterations):
+# Calculate Hessian
+H = np.nan * np.ones((2*filter_lenght, 2*filter_lenght))
+for i in range(0,filter_lenght): # kappa
+    for j in range(0, filter_lenght): # kappa kappa
+        H[i, j] = 2 * np.sum(I[:,i]*I[:,j])
+    for j in range(filter_lenght, 2*filter_lenght): # kappa eta
+        H[i, j] = 2 * np.sum(I[:,i]*A[:,j-filter_lenght])
+for i in range(filter_lenght, 2*filter_lenght): # eta
+    for j in range(0, filter_lenght): # eta kappa
+        H[i,j] = 2 * np.sum(A[:,i-filter_lenght]*I[:,j])
+    for j in range(filter_lenght, 2*filter_lenght): # eta eta
+        H[i,j] = 2 * np.sum(A[:,i-filter_lenght]*A[:,j-filter_lenght])
+
+def hessian(param):
+    return H
+
+def jacobian(param):
+    u = u_compute(param[0:filter_lenght],param[filter_lenght:2*filter_lenght])
+    J = np.nan * np.ones((2*filter_lenght,))
+    for i in range(0, filter_lenght):
+        J[i] = -2 * np.sum((u_data - u)*I[:,i])
+    for i in range(filter_lenght, 2*filter_lenght):
+        J[i] = -2 * np.sum((u_data - u)*A[:,i-filter_lenght])
+    #pdb.set_trace()
+    return J
+
+
+iterations = 10
+for iterat in range(iterations):
 
     
-    u, a, _ = neuron_fit.simulate(i)
+    u, a, _ = neuron_fit.simulate(intensity)
     error_vmem = error_u(u)
-    print('error_u: {0:.2f}'.format(error_vmem/steps))
+    print('Iteration {0:4} -> Error_u: {1:.2f}'.format(iterat,error_vmem/steps))
 
 
 
@@ -61,7 +100,7 @@ for _ in range(iterations):
 
     # Input
     plt.subplot(4,1,1)
-    plt.plot(t, i)
+    plt.plot(t, intensity)
     plt.title('I(t)')
 
     # Filters
@@ -92,7 +131,8 @@ for _ in range(iterations):
     plt.show()
 
     param = np.concatenate((neuron_fit.kappa, neuron_fit.eta), axis=0)
-    result = minimize(error_u_param, param, method ='CG', options={'maxiter':1})
+    result = minimize(error_u_param, param, method ='Newton-CG', options={'maxiter':1}, jac=jacobian)#, hess=hessian)
+    #pdb.set_trace()
     neuron_fit.kappa = result.x[0:filter_lenght]
     neuron_fit.eta = result.x[filter_lenght:]
 
